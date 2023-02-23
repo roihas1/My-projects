3f551bba351416528653ce234b1ae70990948d63
roihas1 <103654352+roihas1@users.noreply.github.com>
2023-02-23 18:43:53 +0200

    Add files via upload

---
+++ Face-recognition/Face recognition.py
@@ -0,0 +1,62 @@
+import cv2
+import face_recognition
+import os
+import numpy as np 
+
+path = 'images'
+images = [] 
+Names = []
+mylist = os.listdir(path)
+# Insert the images name to a list
+for cl in mylist:
+    curimg = cv2.imread(f'{path}/{cl}')
+    images.append(curimg)
+    Names.append(os.path.splitext(cl)[0])
+
+def findEncoding(images):
+    # Convert images to encode and put in a list
+    encode_list=[]
+    for img in images:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encode_list.append(encode)
+    return encode_list
+
+encodeListKnown = findEncoding(images)
+# print('Encoding complete')
+# print(Names)
+# print(mylist)
+cap = cv2.VideoCapture(0)
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+
+    for encodeFace ,faceloc in zip(encodeCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace,tolerance=0.50)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = Names[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1 = faceloc
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-5),cv2.FONT_HERSHEY_PLAIN,1,(255,255,255),2)
+
+    
+
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
+        
+
+
+
+
+
